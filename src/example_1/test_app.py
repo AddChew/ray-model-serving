fbe39ce49d6205da7bc084a558525561feed8b1f
AddChew <addison_chew@outlook.com>
2023-09-19 01:11:49 +0800

    interim commit

--- src/example_1/test_app.py
+++ src/example_1/test_app.py
@@ -7,10 +7,32 @@ import requests
 # print(requests.get(url = url, data = payload).json())
 
 
+# # Application level APIs
 # # API to retrieve information on all serve applications
 # url = 'http://localhost:52365/api/serve/applications/'
 # print(requests.get(url = url).json())
 
+# # API to update serve applications
+# url = 'http://localhost:52365/api/serve/applications/'
+# payload = {
+#   "applications": [
+#     {
+#       "name": "muahahaha",
+#       "route_prefix": "/model",
+#       "import_path": "app:deployment",
+#       "runtime_env": {},
+#       "deployments": [
+#         {"name": "sentiment-analysis", "num_replicas": 2, "user_config": {"model": "distilbert-base-uncased-finetuned-sst-2-english"}}
+#       ]
+#     }
+#   ]
+# }
+# print(requests.put(url = url, data = json.dumps(payload)))
 
-# TODO: explore the use of update deployments via rest api: https://docs.ray.io/en/latest/serve/api/index.html#v2-rest-api-multi-application
+# # API to shutdown all applications
+# url = 'http://localhost:52365/api/serve/applications/'
+# print(requests.delete(url = url))
+
+
+# TODO: explore the use of update deployments via rest api: https://docs.ray.io/en/latest/serve/api/index.html#v1-rest-api-single-application
 # TODO: explore and see if it is possible to update deployments independently, instead of updating all at once when serve deploy is called
