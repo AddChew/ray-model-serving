001c394750a71822030dc2fd9b323d6702b4ec0e
AddChew <addison_chew@outlook.com>
2023-09-19 23:38:07 +0800

    explore v1 api

--- src/example_1/test_app.py
+++ src/example_1/test_app.py
@@ -34,5 +34,29 @@ import requests
 # print(requests.delete(url = url))
 
 
-# TODO: explore the use of update deployments via rest api: https://docs.ray.io/en/latest/serve/api/index.html#v1-rest-api-single-application
+# # Deployment level APIs
+# # API to retrieve information on all serve deployments
+# url = 'http://localhost:52365/api/serve/deployments/'
+# print(requests.get(url = url).json())
+
+# # API to update serve deployments
+# url = 'http://localhost:52365/api/serve/deployments/'
+# payload = {
+#     "route_prefix": "/model",
+#     "import_path": "app:deployment",
+#     "runtime_env": {},
+#     "deployments": [
+#         {"name": "sentiment-analysis", "num_replicas": 2, "user_config": {"model": "distilbert-base-uncased-finetuned-sst-2-english"}}
+#     ]
+# }
+# print(requests.put(url = url, data = json.dumps(payload)))
+
+# # API to get deployment status
+# url = 'http://localhost:52365/api/serve/deployments/status'
+# print(requests.get(url = url).json())
+
+# API to shutdown deployments
+# url = 'http://localhost:52365/api/serve/deployments/'
+# print(requests.delete(url = url))
+
 # TODO: explore and see if it is possible to update deployments independently, instead of updating all at once when serve deploy is called
