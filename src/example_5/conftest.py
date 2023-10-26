d3ba4470433b3c3ea8ae6d6b4c335b1db25b4975
AddChew <addison_chew@outlook.com>
2023-10-27 01:37:52 +0800

    add no fixture eg

--- src/example_5/conftest.py
+++ src/example_5/conftest.py
@@ -6,6 +6,6 @@ from ray import serve
 
 @pytest.fixture(scope = "session")
 def ray_serve():
-    ray.init(num_cpus = 1)
+    ray.init(num_cpus = 1, ignore_reinit_error = True)
     yield serve.start(detached = True, http_options = {"host": "0.0.0.0"})
     ray.shutdown()
--- src/example_5/test_app.py
+++ src/example_5/test_app.py
@@ -1,3 +1,4 @@
+import ray
 import pytest
 import requests
 
@@ -23,7 +24,7 @@ class TestSentimentAnalysisDeployment:
         }
 
 
-class TestSentimentAnalysisServer:
+class TestSentimentAnalysisServerFixture: # This method is preferred if we want to reuse the same ray cluster across tests from different classes
 
     def __setup_endpoint(self):
         serve.run(
@@ -34,6 +35,40 @@ class TestSentimentAnalysisServer:
         endpoint = f"{ray_serve.root_url}/model/predict"
         self.__setup_endpoint()
 
+        response = requests.post(endpoint, params = {"input_text": ""})
+        assert response.status_code == 200
+        assert response.json() == {'message': 'Please provide input_text for inference.'}
+
+        response = requests.post(endpoint, params = {"input_text": "happy"})
+        assert response.status_code == 200
+        assert response.json() == {
+            "sentiment": {
+                "label": "POSITIVE",
+                "score": 0.9998753070831299,
+            }, 
+            "model": "distilbert-base-uncased-finetuned-sst-2-english",
+        }
+
+
+class TestSentimentAnalysisServerNoFixture:
+
+    @classmethod
+    def setup_class(cls):
+        cls.host = "0.0.0.0"
+        cls.port = "8000"
+        ray.init(num_cpus = 1, ignore_reinit_error = True)
+        serve.run(
+            SentimentAnalysis.options(ray_actor_options = {"num_cpus": 0}).bind(), 
+            host = cls.host,
+            port = cls.port,
+        )
+
+    @classmethod
+    def teardown_class(cls):
+        ray.shutdown()
+    
+    def test_predict_server_response(self):
+        endpoint = f"http://{self.host}:{self.port}/model/predict"
         response = requests.post(endpoint, params = {"input_text": ""})
         assert response.status_code == 200
         assert response.json() == {'message': 'Please provide input_text for inference.'}
