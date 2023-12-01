6906068582156686ebbb41e47784b49f8dd1b87b
AddChew <addison_chew@outlook.com>
2023-12-02 00:24:02 +0800

    add deployment

--- src/example_7/app.py
+++ src/example_7/app.py
@@ -1,21 +1,50 @@
+import logging
 from ray import serve
+from datetime import datetime
 from fastapi import FastAPI
-from factory import DummyModelPipeline, actor
 
 
 app = FastAPI()
 
 
+class DummyModelPipeline:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return "I am a dummy model pipeline"
+
+
+@serve.deployment
+class DummyShapPipeline:
+
+    def __init__(self, name, *args, **kwargs):
+        self.name = name
+        self.logger = logging.getLogger("ray.serve")
+
+    def write_to_file(self): # TODO: integrate with tenacity retry, add logging for attempt and create flaky function
+        self.logger.info(self.name)
+        time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        filepath = f'./{time}.txt'
+        with open(filepath, "w") as f:
+            f.write(time + " " + self.name)
+        return "Write to file successfully"
+    
+
 @serve.deployment(name = 'sentiment-analysis', route_prefix = '/model', num_replicas = 1)
 @serve.ingress(app)
 class SentimentAnalysis:
 
-    def __init__(self):
+    def __init__(self, explainer):
         self._classifier = DummyModelPipeline()
+        self._explainer = explainer
 
     @app.post("/predict")
     async def predict(self, input_text: str) -> str:
-        actor.write_to_file.remote()
+        self._explainer.write_to_file.remote()
         return self._classifier(input_text)
 
-deployment = SentimentAnalysis.bind()
+
+explainer = DummyShapPipeline.bind("hi")
+model = SentimentAnalysis.bind(explainer)
--- src/example_7/deploy.sh
+++ src/example_7/deploy.sh
@@ -2,4 +2,4 @@
 
 source activate model-serving
 ray start --head --dashboard-port 8001 --metrics-export-port 8002
-serve run app:deployment -p 8003
+serve run app:model -p 8003
