fc149e6eccb778b2e7210ab890b8beac698a6c73
AddChew <addison_chew@outlook.com>
2023-12-04 12:38:09 +0800

    figure out how to pass logger to decorator

--- src/example_7/app.py
+++ src/example_7/app.py
@@ -1,10 +1,25 @@
 import logging
+import numpy as np
 from ray import serve
 from datetime import datetime
 from fastapi import FastAPI
+from tenacity import retry, stop_after_attempt, wait_random
 
 
 app = FastAPI()
+
+
+logger = logging.getLogger("ray.serve")
+
+
+def custom_before_log(retry_state):
+    logger.info(f"Attempt: {retry_state.attempt_number}")
+
+
+@retry(stop = stop_after_attempt(3), wait = wait_random(1, 2), before = custom_before_log)
+def do_something():
+    if np.random.uniform >= 0.1:
+        raise Exception("Something went wrong!")
 
 
 class DummyModelPipeline:
@@ -23,13 +38,15 @@ class DummyShapPipeline:
         self.name = name
         self.logger = logging.getLogger("ray.serve")
 
-    def write_to_file(self): # TODO: integrate with tenacity retry, add logging for attempt and create flaky function
+    def write_to_file(self):
+        do_something() # TODO: Figure out how to pass logger        
         self.logger.info(self.name)
         time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
         filepath = f'./{time}.txt'
         with open(filepath, "w") as f:
             f.write(time + " " + self.name)
         return "Write to file successfully"
+        
     
 
 @serve.deployment(name = 'sentiment-analysis', route_prefix = '/model', num_replicas = 1)
