346cb2d1558640acb6b6e75986525e1777a3557b
AddChew <addison_chew@outlook.com>
2023-12-23 22:18:28 +0800

    update deploy script

--- src/fastapi-django/deploy.sh
+++ src/fastapi-django/deploy.sh
@@ -3,6 +3,5 @@
 source activate model-serving
 python manage.py makemigrations demo
 python manage.py migrate
-uvicorn app:app --port 8000
-# ray start --head --dashboard-port 8001 --metrics-export-port 8002
-# serve run app:model -p 8003
+ray start --head --dashboard-port 8001 --metrics-export-port 8002
+serve run app:deployment -p 8003
