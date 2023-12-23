ec9dc96fad1e3be58d9366f3250f1041c831b9ac
AddChew <addison_chew@outlook.com>
2023-12-23 22:05:18 +0800

    reorder mount

--- src/example_8/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source activate model-serving
-ray start --head --dashboard-port 8001 --metrics-export-port 8002
-serve run app:deployment -p 8003
--- src/fastapi-django/app.py
+++ src/fastapi-django/app.py
@@ -5,16 +5,13 @@ from importlib.util import find_spec
 from fastapi.staticfiles import StaticFiles
 
 from fastapi.middleware.wsgi import WSGIMiddleware
-from fastapi_django.wsgi import get_wsgi_application
+from fastapi_django.wsgi import application
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fastapi_django.settings")
 
 
 app = FastAPI()
-application = get_wsgi_application()
-
-app.mount("/", WSGIMiddleware(application))
 app.mount("/static",
     StaticFiles(
          directory = os.path.normpath(
@@ -23,3 +20,4 @@ app.mount("/static",
    ),
    name = "static",
 )
+app.mount("/", WSGIMiddleware(application))
