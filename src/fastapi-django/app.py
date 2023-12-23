f98b28e2b185b7df4add3d389b732a0c11efa5e7
AddChew <addison_chew@outlook.com>
2023-12-23 22:16:24 +0800

    use ray serve

--- src/fastapi-django/app.py
+++ src/fastapi-django/app.py
@@ -1,23 +1,42 @@
 import os
 
+from ray import serve
 from fastapi import FastAPI
 from importlib.util import find_spec
 from fastapi.staticfiles import StaticFiles
 
-from fastapi.middleware.wsgi import WSGIMiddleware
 from fastapi_django.wsgi import application
+from fastapi.middleware.wsgi import WSGIMiddleware
+from ray.serve._private.http_util import BufferedASGISender
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fastapi_django.settings")
 
 
-app = FastAPI()
-app.mount("/static",
-    StaticFiles(
-         directory = os.path.normpath(
-              os.path.join(find_spec("django.contrib.admin").origin, "..", "static")
-         ),
-   ),
-   name = "static",
-)
-app.mount("/", WSGIMiddleware(application))
+def setup_app():
+     app = FastAPI()
+     app.mount("/static",
+     StaticFiles(
+          directory = os.path.normpath(
+               os.path.join(find_spec("django.contrib.admin").origin, "..", "static")
+          ),
+     ),
+     name = "static",
+     )
+     app.mount("/", WSGIMiddleware(application))
+     return app
+    
+
+@serve.deployment(num_replicas = 2)
+class Deployment:
+    
+    def __init__(self):
+        self.app = setup_app()
+
+    async def __call__(self, request):
+        sender = BufferedASGISender()
+        await self.app(request.scope, receive = request.receive, send = sender)
+        return sender.build_asgi_response()
+    
+
+deployment = Deployment.bind()
